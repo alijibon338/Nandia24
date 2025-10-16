b3a6e981e2cf4b90d60def02f9d2aa7386921292
alijibon338 <alijibon338@gmail.com>
2025-10-16 13:19:44 +0300

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+apiKey: "AIzaSyBUty0az6KiYCoP1b5egrGocD_SmygT9cg",
+authDomain: "nandia24-12a84.firebaseapp.com",
+databaseURL: "https://nandia24-12a84-default-rtdb.firebaseio.com",
+projectId: "nandia24-12a84",
+storageBucket: "nandia24-12a84.firebasestorage.app",
+messagingSenderId: "138926004379",
+appId: "1:138926004379:web:07fd99527689f92a8a8e3e",
+measurementId: "G-6277TEMPKW"
+};
